08769231047f7b93676ef80f7d32ab46611102f9
Miguel Angel S <massanchez27@gmail.com>
2020-08-15 14:06:21 -0400

    perfil en produccion

--- index.js
+++ index.js
@@ -184,3 +184,16 @@ if (user != null) {
 
 }
 
+var password = document.getElementById("inputPass")
+  , confirm_password = document.getElementById("InputRepass");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
